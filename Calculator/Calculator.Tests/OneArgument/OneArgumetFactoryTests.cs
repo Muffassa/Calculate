8cc6ec0d817e22c31ac63e3a213190dbb58b7bac
Muffassa <keddofilatov@gmail.com>
2015-07-09 12:53:43 +0600

    test for factory added

--- Calculator/Calculator.Tests/OneArgument/OneArgumetFactoryTests.cs
+++ Calculator/Calculator.Tests/OneArgument/OneArgumetFactoryTests.cs
@@ -1,6 +1,21 @@
-﻿namespace Calculator.Tests.OneArgument
+﻿using System;
+using Calculator.OneArgument;
+using NUnit.Framework;
+
+namespace CalculatorTests.OneArgument
 {
-    class OneArgumetFactory
+    [TestFixture]
+    class OneArgumetFactoryTests
     {
+        [TestCase(typeof(Absolute), "Abs")]
+        [TestCase(typeof(Cos),"Cos")]
+        [TestCase(typeof(Sin), "Sin")]
+        [TestCase(typeof(Factorial), "Factorial")]
+        [TestCase(typeof(Sqrt), "Sqrt")]
+        public void CreateCalculator(Type expectedTypeOfOperation, string nameOfOperation )
+        {
+            Type resultType = OneArgumentFactory.CreateCalculator(nameOfOperation).GetType();
+            Assert.AreEqual(expectedTypeOfOperation, resultType);
+        }
     }
 }
